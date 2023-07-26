c7e180f69dec58745bf02ebd152c74e62fafe4d4
Kevin Kariuki <vingentz@gmail.com>
2023-07-26 15:04:14 +0300

    Sort in ascending order using Bubble Sort Algorithm

--- sort.h
+++ sort.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
+/**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
  * @prev: Pointer to previous element of the list
